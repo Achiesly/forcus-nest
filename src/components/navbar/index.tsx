66c7749f7bb8577c20e248c56b1b33c4469ff649
Sylvia Achieng <achiengsylvia20@gmail.com>
2025-07-01 02:41:36 +0300

    update

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -12,7 +12,7 @@ export default function Navbar({ className = "" }: NavbarProps) {
 
     return (
         <nav className={className}>
-            <div className="flex items-center justify-between p-4">
+            <div className="flex items-center justify-between p-6 px-4 sm:px-2 md:px-1 lg:px-3 mb-4 md:mb-0">
                 <Link href="/">
                     <Logo />
                 </Link>
